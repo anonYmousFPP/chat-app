9983f9e5138682eaeb9b5251a769b9b8da801b59
abhay <abhaygaming14122001@gmail.com>
2025-06-04 13:16:04 +0530

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,50 @@
+import { WebSocket, WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+interface User {
+    socket: WebSocket;
+    room : string;
+}
+let allSockets : User[] = [];
+
+wss.on("connection", (socket) => {
+    socket.on("message", (message) => {
+        //@ts-ignore
+        const parsedMessage = JSON.parse(message);
+        console.log(parsedMessage);
+        console.log(typeof(parsedMessage));
+        if(parsedMessage.type === "join"){
+        console.log(parsedMessage.payload.roomId);
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId
+            });
+        }
+
+
+        if(parsedMessage.type == "chat"){
+            //@ts-ignore
+            let currentUserRoom = null;
+            for(let i=0; i<allSockets.length; i++){
+                if(allSockets[i].socket === socket){
+                    currentUserRoom = allSockets[i].room;
+                }
+            }
+
+            console.log(currentUserRoom);
+            for(let i=0; i<allSockets.length; i++){
+                if(allSockets[i].room == currentUserRoom){
+                    console.log(parsedMessage.payload.message);
+                    allSockets[i].socket.send(parsedMessage.payload.message);
+                }
+            }
+        }
+    });
+
+
+    // socket.on("disconnect", () => {
+    //     console.log("Client disconnected");
+    //     allSockets = allSockets.filter(s => s !== socket);
+    // });
+});
